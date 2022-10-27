0a8c50bdebb60c93fbdf66679d7d0d2f320426ef
rafiki <71207415+rafikidota@users.noreply.github.com>
2022-10-27 19:33:43 -0400

    not working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'CopextelTest';
+
+  ngOnInit(): void {
+      // ($('#example') as any).DataTable();
+      // ($('#example') as any).DataTable({
+      //     stateSave: true,
+      // });
+  }
 }
